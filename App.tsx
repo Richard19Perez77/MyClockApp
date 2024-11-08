5d546801063a2938007ba4268c609ebc66f15fb7
richa <richard19.perez77@yahoo.com>
2024-11-08 17:51:59 +0900

    change default string to date

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.timeText}>
+        {currentTime.toLocaleString('en-US', {
+          hour12: false,
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+          fractionalSecondDigits: 3,
+        })}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +35,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  timeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
