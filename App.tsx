1ad37324e38ce42a86e76532016ed426de56772b
richa <richard19.perez77@yahoo.com>
2024-11-08 19:48:58 +0900

    ready for color tone selection

--- App.tsx
+++ App.tsx
@@ -4,8 +4,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const generateRandomColor = () => {
+    let g = Math.floor(Math.random() * 256)
+    let r = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    // start code for warm vs cool colors
+    // let b = Math.floor(Math.random() * (r + 1));
+    // let r = Math.floor(Math.random() * (b + 1)); 
+    const randomColor = `rgb(${r}, ${g}, ${b})`;
+    return randomColor;
+  };
+
   const [fontsLoaded, setFontsLoaded] = useState(false);
   const [currentTime, setCurrentTime] = useState(new Date());
+  const [backgroundColor, setBackgroundColor] = useState(generateRandomColor());
 
   useEffect(() => {
     const loadFonts = async () => {
@@ -21,6 +33,7 @@ export default function App() {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentTime(new Date());
+      setBackgroundColor(generateRandomColor());
     }, 16);
 
     return () => clearInterval(interval);
@@ -31,16 +44,15 @@ export default function App() {
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
     const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
-    console.log(`${hours}:${minutes}:${seconds}.${milliseconds}`);
     return `${hours}:${minutes}:${seconds}.${milliseconds}`;
   };
 
   if (!fontsLoaded) {
-    return <View><Text>Loading...</Text></View>; 
+    return <View><Text>Loading...</Text></View>;
   }
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor }]}>
       <Text style={[styles.timeText, { fontFamily: 'SpaceMono-Regular' }]}>{formatTime(currentTime)}</Text>
       <StatusBar style="auto" />
     </View>
@@ -50,7 +62,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
